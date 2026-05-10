b5a9990279bdfa6ac099d2edd4a2bc6bba7eab0a
yigitgulyurt <109045170+yigitgulyurt@users.noreply.github.com>
2026-05-10 14:52:45 +0300

    Add image subdomain blueprint and nginx config
    Introduce an images blueprint to serve static images with CORS and long-term caching, register it in the Flask app, and add nginx server blocks for image.yigitgulyurt.net.tr. Changes: new app/routes/images.py (serves files from static/img with Access-Control-Allow-Origin and Cache-Control headers), register images_bp in app/__init__.py (subdomain 'image'), and append nginx config to yigitgulyurt.conf to handle SSL, redirects, OPTIONS responses, and set appropriate headers for the image subdomain.

---
+++ app/routes/images.py
@@ -0,0 +1,23 @@
+"""
+app/routes/images.py
+Görsel servis sistemi
+Subdomain: image.yigitgulyurt.net.tr
+"""
+
+import os
+from flask import Blueprint, request, Response, current_app, send_from_directory, abort
+
+images_bp = Blueprint('images', __name__, subdomain='image')
+
+
+@images_bp.route('/<path:filename>')
+def serve_image(filename):
+    """Görsel dosyalarını servis eder."""
+    images_dir = os.path.join(current_app.root_path, 'static', 'img')
+    if not os.path.exists(os.path.join(images_dir, filename)):
+        abort(404)
+    
+    response = send_from_directory(images_dir, filename)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Cache-Control'] = 'public, max-age=31536000, immutable'
+    return response
